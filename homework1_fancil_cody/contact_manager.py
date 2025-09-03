3bde27bbd2e6fe4c50d832d0204c69fd92d8ac70
Cody Fancil <hounddog51505@gmail.com>
2025-09-03 04:29:10 -0400

    more progress

--- homework1_fancil_cody/contact_manager.py
+++ homework1_fancil_cody/contact_manager.py
@@ -1,12 +1,16 @@
-import datetime , sys
-x = datetime.datetime.now()
+import datetime , sys, os
 # Global database - this is where ALL contacts are stored
 contact_data = {}
+#Initializing variables
 address = {}
 first_name = ""
 last_name = ""
 email = ""
 phone = ""
+# clear screen helps me to keep program clean by clearing previous outputs
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
 def create_contact():
     print("\nCreating a new contact...\n")
     
@@ -17,9 +21,11 @@ def create_contact():
             print("First name cannot be empty.")
             continue
         elif first_name.isdigit():
+            #checks if input is only numbers
             print("First name cannot be number.")
             continue
         break
+    clear_screen()
     while True:
         last_name = input("Enter last name: ").strip().capitalize()
         if last_name == "":
@@ -29,7 +35,9 @@ def create_contact():
             print("Last name cannot be number.")
             continue
         break
+    clear_screen()
     while True:
+        # Strip and capitalize trims whitespace and capitalizes first letter
         email = input("Enter email: ").strip().capitalize()
         if email == "":
             print("Email cannot be empty.")
@@ -41,15 +49,18 @@ def create_contact():
             print("Email cannot be only numbers.")
             continue
         break
+    clear_screen()
     while True:
         phone = input("Enter phone number: ")
         if phone == "":
             print("Phone number cannot be empty.")
             continue
         elif phone.isalpha():
+            #checks if input is only letters
             print("Phone number cannot be letters.")
             continue
         break
+    clear_screen()
     while True:
         street = input("Enter street: ").strip().capitalize()
         if street == "":
@@ -59,6 +70,7 @@ def create_contact():
             print("Street cannot be only numbers.")
             continue
         break
+    clear_screen()
     while True:
         city = input("Enter city: ").strip().capitalize()
         if city == "":
@@ -68,6 +80,7 @@ def create_contact():
             print("City cannot be only numbers.")
             continue
         break
+    clear_screen()
     while True:
         state = input("Enter state: ").strip().capitalize()
         if state == "":
@@ -77,6 +90,7 @@ def create_contact():
             print("State cannot be only numbers.")
             continue
         break
+    clear_screen()
     while True:
         zip_code = input("Enter zip code: ").strip()
         if zip_code == "":
@@ -86,6 +100,7 @@ def create_contact():
             print("Zip code cannot be letters.")
             continue
         break
+    clear_screen()
     
     # Creating the contact dictionary then returning it
     
@@ -94,6 +109,7 @@ def create_contact():
                "state": state, 
                "zip code": zip_code}
     
+    x = datetime.datetime.now()
     contact_data = {"first_name": first_name, 
                     "last_name": last_name, 
                     "email": email, 
@@ -102,6 +118,7 @@ def create_contact():
                     "Timestamp": f"Contact created on {x}"
                     }
     
+    # Generating a unique contact ID
     contact_id = f"contact_{len(contacts_database) + 1}"
     
     contacts_database[contact_id] = contact_data
@@ -116,19 +133,25 @@ contacts_database = {}
 
 def show_contacts():
     print("\n--- All Contacts ---")
+    
     for contact_id, info in contacts_database.items():
         print(f"{contact_id}: {info['first_name']} , {info['last_name']} , {info['email']} , {info['phone']} , {info['address']} , {info['Timestamp']}")
+        
 def delete_contact():
     print("\nDeleting a contact...\n")
-    contact_id = input("Enter the contact ID to delete (e.g., contact_001): ").strip()
+    
+    contact_id = input("Enter the contact ID to delete (e.g., contact_1, etc.): ").strip()
+    
     if contact_id in contacts_database:
         del contacts_database[contact_id]
         print(f"Contact {contact_id} has been deleted.")
+    elif not contacts_database:
+        print("\nNo contacts are stored currently.\n")
     else:
         print(f"Contact ID {contact_id} not found.")
 
 def main_menu():
-    
+    clear_screen()
     # Main menu loop
     while True:
         print("\nContact Manager:\n")
@@ -143,15 +166,18 @@ def main_menu():
         print("9. Exit")
         
         # Get user choice
-        choice = int(input("Choose an option (1-9): "))
+        choice = int(input("\nChoose an option (1-9): "))
         
         # Handle user choice
         if choice == 1:
+            clear_screen()
             create_contact()
         elif choice == 2:
+            clear_screen()
             # search_contacts()
             pass
         elif choice == 3:
+            clear_screen()
             while True:
                 if not contacts_database:
                     print("\nNo contacts are stored currently.\n")
@@ -160,23 +186,30 @@ def main_menu():
                     show_contacts()
                     break
         elif choice == 4:
+            clear_screen()
             # update_contact()
             pass
         elif choice == 5:
+            clear_screen()
             delete_contact()
         elif choice == 6:
+            clear_screen()
             # generate_statistics()
             pass
         elif choice == 7:
+            clear_screen()
             # find_duplicates()
             pass
         elif choice == 8:
+            clear_screen()
             # export_by_category()
             pass
         elif choice == 9:
-            print("Exiting the program. Goodbye!")
+            clear_screen()
+            print("\nExiting the program. Goodbye!\n")
             sys.exit()
-        else:
-            print("Invalid choice. Please select a valid option.")
+        else:  
+            clear_screen()
+            print("\nInvalid choice. Please select a valid option.\n")
             
 main_menu()
